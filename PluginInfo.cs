f821d6e15efe143a422cb0f4f80a0e611be6708f
Cdev <colindarroch.junk@gmail.com>
2026-03-08 08:07:12 -0700

    Increment version number to 1.2.0
    Updated version number from 1.1.9 to 1.2.0.

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,8 +5,9 @@
         public const string GUID = "Liquid.Client";
         public const string Name = "Liquid.Client";
         public const string Description = "liquid on top creds to imundtrust and cdev";
-        public const string Version = "1.1.9";
+        public const string Version = "1.2.0";
     }
 }
+
 
 
